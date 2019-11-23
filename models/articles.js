e894b34ab1867bbd7dcd303760aa89dafe48923e
Said Jillo <umurjillo@gmail.com>
2019-11-23 12:32:05 +0300

    fixed bugs

--- models/articles.js
+++ models/articles.js
@@ -44,11 +44,9 @@ class Articles {
         })
                                           
     }
-    
 
     // return one article from the database
     async findOne(id){
-        console.log(id);
         let article = await this.client.query("SELECT * FROM articles WHERE articleId= $1", [id]);
         return article.rows[0];
     }
--- routes/articles.js
+++ routes/articles.js
@@ -6,12 +6,9 @@ const articlesCtrl = require('../controllers/articles');
 const auth = require("../middleware/auth");
 
 router.get('/', auth.verifyToken, articlesCtrl.getAllArticles);
-router.get('/:articleId', auth.verifyToken, articlesCtrl.getOneArticle);
 router.get('/:userId', auth.verifyToken, articlesCtrl.getAllArticlesEmployeeSpecific);
-
+router.get('/:articleId', auth.verifyToken, articlesCtrl.getOneArticle);
 router.post('/', auth.verifyToken, articlesCtrl.createArticle);
-router.post('/comments/:articleId', auth.verifyToken, articlesCtrl.createComment);
-router.delete('/comments/:articleId', auth.verifyToken, articlesCtrl.deleteComment);
 router.delete('/:articleId', auth.verifyToken, articlesCtrl.deleteArticle);
 router.put('/:articleId', auth.verifyToken, articlesCtrl.modifyArticle);
 
