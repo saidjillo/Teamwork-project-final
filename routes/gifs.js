26bbbef63705e87b87bb1196f27836b89cdb469f
Said Jillo <umurjillo@gmail.com>
2019-11-24 15:21:52 +0300

    fixed bugs in gif posts

--- routes/gifs.js
+++ routes/gifs.js
@@ -8,6 +8,7 @@ const auth = require("../middleware/auth");
 router.get('/', gifsCtrl.getAllGifs);
 router.get('/:gifId', gifsCtrl.getOneGif);
 router.post('/', gifsCtrl.createGif);
+router.put('/:gifId', gifsCtrl.modifyGif);
 router.delete('/:gifId', gifsCtrl.deleteGif);
 
 
